2914f7e7c5a7339943c448dc86ed03cdbc4674c0
kaoru.i <carriekcn@gmail.com>
2019-10-21 12:03:31 +0900

    updated multiple files

--- app/assets/javascripts/tab-contents.js
+++ app/assets/javascripts/tab-contents.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-  $("span").text("wow");
+$(function(){
+  $('.tab').click(function(){
+    $('.is-active').removeClass('is-active');
+    $(this).addClass('is-active');
+    $('.is-show').removeClass('is-show');
+    // クリックしたタブからインデックス番号を取得
+    const index = $(this).index();
+    // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+    $('.panel').eq(index).addClass('is-show');
+
+    //以下テスト中
+    // $('.is-show').css('background-color', '#fff');
+    // $('.is-show').addClass('test');   
+
+    // $('.is-show').css('display', 'flex');
+    // $('.is-show panel-right').css('flex', '1');
+    // $('.is-show panel-left').css('flex', '1');
+  });
 });
