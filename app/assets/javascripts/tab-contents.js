a26762613c9be3167ebf800f110148968f8f9892
kaoru.i <carriekcn@gmail.com>
2019-10-22 12:36:32 +0900

    Adjustment of html and css

--- app/assets/javascripts/tab-contents.js
+++ app/assets/javascripts/tab-contents.js
@@ -8,12 +8,5 @@ $(function(){
     // クリックしたタブと同じインデックス番号をもつコンテンツを表示
     $('.panel').eq(index).addClass('is-show');
 
-    //以下テスト中
-    // $('.is-show').css('background-color', '#fff');
-    // $('.is-show').addClass('test');   
-
-    // $('.is-show').css('display', 'flex');
-    // $('.is-show panel-right').css('flex', '1');
-    // $('.is-show panel-left').css('flex', '1');
   });
 });
