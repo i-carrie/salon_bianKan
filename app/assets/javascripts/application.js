27df54f08bcebbb94dab85ff332af72577c94977
kaoru.i <carriekcn@gmail.com>
2019-11-18 12:17:02 +0900

    correct & lang setting at application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require rails-ujs
 //= require moment
 //= require fullcalendar
+//= require fullcalendar/lang/ja
 //= require underscore
 //= require gmaps/google
 //= require activestorage
@@ -21,18 +22,27 @@
 //= require_tree .
 
 $(function () {
-  function eventCalendar() {
-      return $('#calendar').fullCalendar({});
-  };
-  function clearCalendar() {
-      $('#calendar').html('');
-  };
+  // 画面遷移を検知
   $(document).on('turbolinks:load', function () {
-    eventCalendar();
-  });
-  $(document).on('turbolinks:before-cache', clearCalendar);
+      // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
+      if ($('#calendar').length) {
+          function eventCalendar() {
+              return $('#calendar').fullCalendar({
+              });
+          };
+          function clearCalendar() {
+              $('#calendar').html('');
+          };
 
-  $('#calendar').fullCalendar({
-    events: '/events.json'
-    });
+          $(document).on('turbolinks:load', function () {
+              eventCalendar();
+          });
+          $(document).on('turbolinks:before-cache', clearCalendar);
+
+          $('#calendar').fullCalendar({
+              events: '/events.json',
+              lang: 'ja'
+          });
+      }
+  });
 });
