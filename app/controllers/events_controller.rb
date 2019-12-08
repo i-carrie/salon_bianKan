77c97337c26dd334e1800c688470f4c139133099
kaoru.i <carriekcn@gmail.com>
2019-12-08 17:41:27 +0900

    update multiple files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index
 
   # GET /events
   # GET /events.json
@@ -15,6 +16,7 @@ class EventsController < ApplicationController
   # GET /events/new
   def new
     @event = Event.new
+
   end
 
   # GET /events/1/edit
@@ -70,5 +72,9 @@ class EventsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:title, :description, :start_date, :end_date)
+    end
+
+    def move_to_index
+      redirect_to  root_path unless user_signed_in?
     end
 end
